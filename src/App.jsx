5156461096eceb4503013d4006ee34b2973121df
Logesh kumar G <logeshkumarbtech@gmail.com>
2024-03-26 23:34:59 +0530

    Weater app logiv

---
+++ src/App.jsx
@@ -0,0 +1,100 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const SearchBar = ({ onSearch }) => {
+  const [city, setCity] = useState("");
+
+  const handleSearch = () => {
+    onSearch(city);
+  };
+
+  return (
+    <div className="search-bar">
+      <input
+        type="text"
+        value={city}
+        onChange={(e) => setCity(e.target.value)}
+        placeholder="Enter city name"
+      />
+      <button onClick={handleSearch}>Search</button>
+    </div>
+  );
+};
+
+const WeatherDisplay = ({ city }) => {
+  const [weatherData, setWeatherData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    if (city) {
+      setLoading(true);
+      axios
+        .get(
+          `https://api.weatherapi.com/v1/current.json?key=4b8bfcd636394b9eb09140114243001&q=${city}`
+        )
+        .then((res) => {
+          console.log(res.data)
+          setWeatherData(res.data);
+        })
+        .catch((err) => {
+          console.error("Error fetching data");
+          alert("Failed to fetch weather data");
+        })
+        .finally(() => {
+          setLoading(false);
+        });
+    }
+  }, [city]);
+  return (
+    <div className="weather-display">
+      {loading && <p>Loading data...</p>}
+      {!loading && weatherData && (
+        <div className="weather-cards">
+          <WeatherCard
+            title="Temperature"
+            data={`${weatherData.current.temp_c}\u00B0C`}
+          />
+          <WeatherCard
+            title="Humidity"
+            data={`${weatherData.current.humidity}\u00B0C`}
+          />
+          <WeatherCard
+            title="Condition"
+            data={`${weatherData.current.condition.text}`}
+          />
+          <WeatherCard
+            title="Wind Speed"
+            data={`${weatherData.current.wind_kph} kph`}
+          />
+        </div>
+      )}
+    </div>
+  );
+};
+
+const WeatherCard = ({ title, data }) => {
+  return (
+    <div className="weather-card">
+      <h3>{title}</h3>
+      <p>{data}</p>
+    </div>
+  );
+};
+
+function App() {
+  const [city, setCity] = useState("");
+
+  const handleSearch = (searchCity) => {
+    setCity(searchCity);
+  };
+
+  return (
+    <div className="App">
+      <SearchBar onSearch={handleSearch} />
+      <WeatherDisplay city={city} />
+    </div>
+  );
+}
+
+export default App;
